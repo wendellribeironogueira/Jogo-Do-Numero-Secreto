f2e7864d8c4f6730d05aefdbc3c751e7976699c2
Wendell Ribeiro Nogueira <wenri22@gmail.com>
2025-10-17 23:21:32 -0300

    correções de ortografia

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('h1', 'Advinhe o Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um Número entre 1 e 10');
 }
 exibirMensagemInicial();
 
@@ -28,7 +28,7 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela ('h1', 'Você Acertou!');
         let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}!`; 
+        let mensagemTentativas = `Você Descobriu o Número Secreto com ${tentativas} ${palavraTentativas}!`; 
                 exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById ('reiniciar').removeAttribute ('disabled');
     } else if (chute > numeroSecreto) {
@@ -36,7 +36,7 @@ function verificarChute() {
         exibirTextoNaTela ('p', 'O Número Secreto é Menor que o Chute!');
     } else if (chute < numeroSecreto) {
         exibirTextoNaTela ('h1', 'Você Não Acertou!');
-        exibirTextoNaTela ('p', 'O Número Secreto é Maior que o chute!');
+        exibirTextoNaTela ('p', 'O Número Secreto é Maior que o Chute!');
     }
     tentativas++;
     limparCampo();
